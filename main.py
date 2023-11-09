fb80a2817c73f6b6863efe4f333b19c2f2299637
RoXtra1 <skornyakovartem@mail.ru>
2023-11-10 00:12:07 +0300

    добавлена бета версия функции оптимального расположения башен в городе

--- main.py
+++ main.py
@@ -1,36 +1,86 @@
-from random import random                   # для случайного определения блоков, недоступных для расположения башен
-import matplotlib.pyplot as plt             # для графического отображения
-import numpy as np                          # для создания координатной матрицы
+from random import random           # для случайного определения блоков, недоступных для расположения башен
+import matplotlib.pyplot as plt     # для графического отображения
+import numpy as np                  # для создания координатной матрицы
 
 
 class CityGrid:
-    def __init__(self, rows, cols, block_prob):
-        self.rows = rows
-        self.cols = cols
-        self.towers = []
-        self.grid = [[0 for _ in range(cols)] for _ in range(rows)]  # Создаем пустую сетку
+    def __init__(self, size, block_prob=0.3):
+        self.size = size    # добавлена для упрощенного присваивания размера квадратной сетке
+        self.rows = size
+        self.cols = size
+        self.grid = [[0 for _ in range(size)] for _ in range(size)]  # создаем пустую сетку где все блоки доступны
 
-        for i in range(rows):               # Заблокированные блоки
-            for j in range(cols):
-                if random() < block_prob:   # Задаем вероятность блокировки квартала
-                    self.grid[i][j] = 1
+        self.towers = []            # список башен
+        self.unavailable = []       # список координат недоступных районов
+        self.rng = 2
 
-    def add_tower(self, row, col, range):
-        tower = (row, col, range)
+        for y in range(size):                        # Заблокированные кварталы
+            for x in range(size):
+                if random() <= block_prob:           # Задаем вероятность блокировки квартала
+                    self.grid[y][x] = 1              # 1 - обозначает черный, недоступный район
+                    self.unavailable.append((y, x))  # сохраняем координаты недоступных районов
+
+    def add_tower(self, row, col):  # добавляем башню в список
+        tower = (row, col, self.rng)
         self.towers.append(tower)
 
-    def visualize(self):
-        plt.imshow(self.grid, cmap='Greys')  # Отображаем сетку в черно-белом формате
+    def is_covered(self, row, col): # проверяем покрыт ли блок хотя бы одной из существующих башен
+        for tower in self.towers:
+            t_row, t_col, rng = tower
+            if abs(t_row - row) <= rng and abs(t_col - col) <= rng:
+                return True
+        return False
 
-        for tower in self.towers:            # Визуализация покрытия вышек
-            row, col, range = tower
-            x, y = np.meshgrid(np.arange(col - range, col + range + 1), np.arange(row - range, row + range + 1))
-            plt.scatter(x, y, color='red', marker='s')
+    def optimize_placement(self):
+        while self.unavailable:                         # пока на поле есть недоступные блоки
+            block = self.unavailable.pop(0)             # берем первый недоступный блок
+            row, col = block
+            if not self.is_covered(row, col):           # если блок не покрыт
+                rng = self.rng
+                size = self.size
+
+                # проходимся по территории вокруг блока на расстоянии радиуса действия башни
+                for y in range(row - rng, row + rng+1):
+                    for x in range(col - rng, col + rng+1):
+
+                        # если блок не за границей, доступен и на нем нет башни
+                        if 0 <= y < size and 0 <= x < size and self.grid[y][x] == 0 and (y,x,self.rng) not in self.towers:
+                            self.add_tower(y, x)            # ставим башню в первую доступную клетку в радиусе действия
+
+                            # Удаляем покрытые блоки из списка недоступных
+                            for i in range(y - rng, y + rng + 1):
+                                for j in range(x - rng, x + rng + 1):
+                                    if (i, j) in self.unavailable:
+                                        self.unavailable.remove((i, j))
+                            break   # заканчиваем поиск подходящего для башни места
+                else:   # если вокруг недоступного блока нет доступных
+                    print("Невозможно покрыть блок башнями с текущим радиусом действия!")
+                    continue
+                break
+
+    def find_most_reliable_path(self, tower1, tower2):
+        # Ваш код для поиска наиболее надежного пути между двумя башнями
+        pass
+
+    def visualize(self):
+        plt.imshow(self.grid, cmap='Greys')                     # Отображаем районы в черно-белом формате
+        for i in range(self.rows+1):
+            plt.axhline(i - 0.5, color='black', linewidth=0.7)  # Горизонтальные линии сетки
+        for j in range(self.cols+1):
+            plt.axvline(j - 0.5, color='black', linewidth=0.7)  # Вертикальные линии сетки
+
+        for tower in self.towers:  # Визуализация покрытия вышек
+            row, col, rng = tower
+            plt.scatter(col, row, color='green', marker='o', s=250)         # отображаем башню (зеленый круг)
+
+            # создаем 2 матрицы для сочетания координат х и у площади покрытия
+            x, y = np.meshgrid(np.arange(col - rng, col + rng + 1), np.arange(row - rng, row + rng + 1))
+            plt.scatter(x, y, color='red', marker='s', alpha=0.4, s=60)     # отображаем зоны действия башен
 
         plt.show()
 
 
 if __name__ == '__main__':
-    city = CityGrid(10, 10, 0.3)    # Создаем сетку размером 10x10 с вероятностью блокировки 0.3
-    city.add_tower(2, 3, 2)              # Размещаем вышку в квартале (2, 3) с диапазоном 2 блока
-    city.visualize()                                    # Визуализация сетки с вышкой и ее покрытием
+    city = CityGrid(10)                     # Создаем сетку размером 20x20 со стандартной вероятностью блокировки 0.3
+    city.optimize_placement()               # оптимально располагаем башни
+    city.visualize()                        # Визуализация сетки с башнями и покрытием
